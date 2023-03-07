333fe985bfe05c99897c79acd4e055d7067fadf3
ashutosh <ashut@TIGER>
2023-03-07 23:14:37 +0530

    Add project files.

---
+++ GHOST/GHOST.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		GHOST.ino
+ Created:	3/7/2023 11:06:13 PM
+ Author:	ashut
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "display.h"
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ GHOST/display.h
@@ -0,0 +1,17 @@
+// display.h
+
+
+
+
+#ifndef _DISPLAY_h
+#define _DISPLAY_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
